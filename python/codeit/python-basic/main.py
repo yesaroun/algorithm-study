6fff9ee37cbdc93706bfaa0387031044918bd0be
akor11lyt <akor1@naver.com>
2022-08-03 01:42:34 +0900

    dictonary study

--- python/codeit/python-basic/main.py
+++ python/codeit/python-basic/main.py
@@ -1,25 +1,3 @@
-# 사전 (dictionary) (중괄호를 열고 닫으면 사전임)
-# key-value pair (키-값 쌍)
-my_dictionary = {
-    5: 25,      # 키가 5이고 값이 25이다.
-    2: 4,
-    3: 9
-}
-print(type(my_dictionary))
-#--==>> <class 'dict'>
-
-print(my_dictionary[3])
-#--==>> 9
-
-# 새로운 쌍을 추가하고 싶을때
-my_dictionary[9] = 81       # 9에 81 추가
-print(my_dictionary)
-#--==>> {5: 25, 2: 4, 3: 9, 9: 81}
-
-# 리스트와 사전의 차이점
-# 리스트는 인덱스가 순서대로 출력 / 리스트의 인덱스는 정수값
-# 사전은 순서라는 개념이 없다. / 사전의 키는 정수형일 필요가 없다
-
 my_family = {
     'mom': 'kim',
     'dad': 'lee',
@@ -27,7 +5,55 @@ my_family = {
     'daughter': 'park'
 }
 
-print(my_family)
-#--==>> {'mom': 'kim', 'dad': 'lee', 'son': 'son', 'daughter': 'park'}
-print(my_family['mom'])
-#--==>> kim
+# 사전에 어떤 값이 있는지 목룍이 필요할때
+print(my_family.values())
+#--==>> dict_values(['kim', 'lee', 'son', 'park'])
+print("kim" in my_family.values())
+#--==>> True
+print("jung" in my_family.values())
+#--==>> False
+
+
+for value in my_family.values():
+    print(value)
+#--==>>
+'''
+kim
+lee
+son
+park
+'''
+
+print(my_family.keys())
+#--==>> dict_keys(['mom', 'dad', 'son', 'daughter'])
+
+for key in my_family.keys():
+    print(key)
+#--==>>
+'''
+mom
+dad
+son
+daughter
+'''
+
+for key in my_family.keys():
+    value = my_family[key]
+    print(key, value)
+#--==>>
+'''
+mom kim
+dad lee
+son son
+daughter park
+'''
+
+for key, value in my_family.items():
+    print(key, value)
+#--==>>
+'''
+mom kim
+dad lee
+son son
+daughter park
+'''
