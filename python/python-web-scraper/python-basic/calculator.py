6d0726d7e073d8d3ed6307664319cd6ae84450d4
akor11lyt <akor1@naver.com>
2022-09-01 00:49:51 +0900

    calculator

---
+++ python/python-web-scraper/python-basic/calculator.py
@@ -0,0 +1,16 @@
+def plus(a=0, b=0):
+    print(a + b)
+
+def minus(a=0, b=0):
+    print(a - b)
+
+def multi(a=1, b=1):
+    print(a * b)
+
+def divide(a=1, b=1):
+    print(a / b)
+
+plus()
+minus()
+multi()
+divide()
