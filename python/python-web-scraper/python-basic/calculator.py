0a3012907fd117ee4f44f3889d26d65bded381f0
akor11lyt <akor1@naver.com>
2022-09-02 02:33:00 +0900

    while 추가

--- python/python-web-scraper/python-basic/calculator.py
+++ python/python-web-scraper/python-basic/calculator.py
@@ -1,16 +1,32 @@
 def plus(a=0, b=0):
-    print(a + b)
+    print("Result: ", a + b)
 
 def minus(a=0, b=0):
-    print(a - b)
+    print("Result: ", a - b)
 
 def multi(a=1, b=1):
-    print(a * b)
+    print("Result: ", a * b)
 
 def divide(a=1, b=1):
-    print(a / b)
+    print("Result: ", a / b)
 
-plus()
-minus()
-multi()
-divide()
+
+playing = True
+
+while playing:
+    a = int(input("Choose a number:\n"))
+    b = int(input("Choose another one:\n"))
+    operation = input("Choose an operation:\n    Options are: + , - , * or /.\n    Write 'exit' to finish.\n")
+
+    if operation == "exit":
+        playing = False
+    elif operation == "+":
+        plus(a, b)
+    elif operation == "-":
+        minus(a, b)
+    elif operation == "*":
+        multi(a, b)
+    elif operation == "/":
+        divide(a, b)
+    else:
+        break
