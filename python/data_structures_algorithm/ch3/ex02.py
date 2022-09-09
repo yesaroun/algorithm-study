c87f960faa09a63028b3c804fc16baa675474a7b
akor11lyt <akor1@naver.com>
2022-09-09 23:47:14 +0900

    수정

--- python/data_structures_algorithm/ch3/ex02.py
+++ python/data_structures_algorithm/ch3/ex02.py
@@ -1,3 +1,4 @@
+import math
 
 sum_ = 0
 
--- python/data_structures_algorithm/ch3/ex04.py
+++ python/data_structures_algorithm/ch3/ex04.py
@@ -1,7 +1,7 @@
 # list에는 2부터 255까지 정수가 출현한다고 정하기
 
 def solution(lst):
-    freq = {x:lst.count(x) for x in set(lst)}
+    freq = {x : lst.count(x) for x in set(lst)}
     if len(freq) == 1 or len(freq) == len(lst):
         return []
     else:
