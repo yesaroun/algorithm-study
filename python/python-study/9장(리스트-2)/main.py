467fde5b563f8616a23ed5cb65263c9e9d84e501
akor11lyt <akor1@naver.com>
2022-09-17 22:59:33 +0900

    리스트

--- python/python-study/9장(리스트-2)/main.py
+++ python/python-study/9장(리스트-2)/main.py
@@ -1,11 +1,7 @@
-def func2(list):
-    list[0] = 99
-
-values = [0, 1, 2, 3]
-print(values)
-func2(values)
-print(values)
-'''
-[0, 1, 2, 3]
-[99, 1, 2, 3]
-'''
+# colors 리스트의 원소와 cars리스트의 원소가 하나씩 짝지어져서 튜플이 되고
+# 이 튜플이 모여서 리스트가 된다.
+colors = ['white', 'silver', 'black']
+cars = ['bnw5', 'sonata', 'malibu', 'sm5']
+colored_cars = [(x,y) for x in colors for y in cars]
+print(colored_cars)
+#--==>> [('white', 'bnw5'), ('white', 'sonata'), ('white', 'malibu'), ('white', 'sm5'), ('silver', 'bnw5'), ('silver', 'sonata'), ('silver', 'malibu'), ('silver', 'sm5'), ('black', 'bnw5'), ('black', 'sonata'), ('black', 'malibu'), ('black', 'sm5')]
