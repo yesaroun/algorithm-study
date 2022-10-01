4797a99b6a3da5215f38e412528e440803772a82
akor11lyt <akor1@naver.com>
2022-10-02 02:12:50 +0900

    문서화

--- python/codeit/object_oriented_programing/abstract/BankAccount.py
+++ python/codeit/object_oriented_programing/abstract/BankAccount.py
@@ -1,38 +1,32 @@
-# 추상화의 좋은 예시
+"""
+이것은 docstring입니다.
+
+이렇게 여러 줄을 써도 하나의 docstring입니다.
+
+설명하고자 하는 클래스, 변수, 메소드 아래에 docstring을 작성한다.
+"""
 class BankAccount:
+    """은행 계좌 클래스"""
     interest = 0.02
 
     def __init__(self, owner_name, balance):
+        """인스턴스 변수: name(문자열), balance(실수형)"""
         self.owner_name = owner_name
         self.balance = balance
 
     def deposit(self, amount):
+        """잔액 인스턴스 변수 balance를 파라미터 amount만큼 늘려주는 메소드"""
         self.balance += amount
 
     def withdraw(self, amount):
+        """잔액 인스턴스 변수 balance를 파라미터 amount만큼 줄여주는 메소드"""
         if self.balance < amount:
             print("Insufficient balance!")
         else:
             self.balance -= amount
 
     def add_interest(self):
+        """잔액 인스턴스 변수 balance를 이자율만큼 늘려주는 메소드"""
         self.balance *= 1 + BankAccount.interest
 
-# 어디에 쓰는 클래스이고 어떻게 사용할지 직관적으로 알 수 있다.
-
-example_account = BankAccount("Hong Gil dong", 1000)
-
-example_account.add_interest()
-print(example_account.balance)
-#--==>> 1020.0
-
-example_account.deposit(500)
-print(example_account.balance)
-#--==>> 1520.0
-
-example_account.withdraw(2000)
-#--==>> Insufficient balance!
-example_account.withdraw(1000)
-
-print(example_account.balance)
-#--==>> 520.0
+# 클래스를 어떻게 사용할 수 있을지 직관적으로 알 수 있다!
