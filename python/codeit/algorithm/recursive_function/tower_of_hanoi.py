f9a65e4c6ac9d2ad8b24c0e25e4f975ae9f0b108
akor11lyt <akor1@naver.com>
2022-10-02 01:57:22 +0900

    hanoi

---
+++ python/codeit/algorithm/recursive_function/tower_of_hanoi.py
@@ -0,0 +1,31 @@
+def move_disk(disk_num, start_peg, end_peg):
+    print("%d번 원판을 %d번 기둥에서 %d번 기둥으로 이동" % (disk_num, start_peg, end_peg))
+
+def hanoi(num_disks, start_peg, end_peg):
+    # base case : 옮길 원판이 없으면 부분 문제를 나누지 않고 함수를 끝낸다.
+    if num_disks == 0:
+        return
+    else:
+        other_peg = 6 - start_peg - end_peg
+
+        # 1. 가장 큰 원판을 제외하고 나머지 원판들을 start_peg에서 other_peg로 이동
+        hanoi(num_disks - 1, start_peg, other_peg)
+
+        # 2. 가장 큰 원판을 start_peg에서 end_peg로 이동
+        move_disk(num_disks, start_peg, end_peg)
+
+        # 3. 나머지 원판들을 other_peg에서 end_peg로 이동
+        hanoi(num_disks - 1, other_peg, end_peg)
+
+
+# 테스트 코드 (포함하여 제출해주세요)
+hanoi(3, 1, 3)
+'''
+1번 원판을 1번 기둥에서 3번 기둥으로 이동
+2번 원판을 1번 기둥에서 2번 기둥으로 이동
+1번 원판을 3번 기둥에서 2번 기둥으로 이동
+3번 원판을 1번 기둥에서 3번 기둥으로 이동
+1번 원판을 2번 기둥에서 1번 기둥으로 이동
+2번 원판을 2번 기둥에서 3번 기둥으로 이동
+1번 원판을 1번 기둥에서 3번 기둥으로 이동
+'''
