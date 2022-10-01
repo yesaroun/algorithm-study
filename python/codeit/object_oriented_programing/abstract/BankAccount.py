9474cd493c21ff1d106a0895a8375dca62cf1281
akor11lyt <akor1@naver.com>
2022-10-02 02:51:52 +0900

    typehinting

--- python/codeit/object_oriented_programing/abstract/BankAccount.py
+++ python/codeit/object_oriented_programing/abstract/BankAccount.py
@@ -1,32 +1,32 @@
-"""
-이것은 docstring입니다.
-
-이렇게 여러 줄을 써도 하나의 docstring입니다.
-
-설명하고자 하는 클래스, 변수, 메소드 아래에 docstring을 작성한다.
-"""
 class BankAccount:
     """은행 계좌 클래스"""
-    interest = 0.02
+    interest: float = 0.02
 
-    def __init__(self, owner_name, balance):
+    def __init__(self, owner_name: str, balance: float) -> None:
         """인스턴스 변수: name(문자열), balance(실수형)"""
         self.owner_name = owner_name
         self.balance = balance
 
-    def deposit(self, amount):
+    def deposit(self, amount: float) -> None:
         """잔액 인스턴스 변수 balance를 파라미터 amount만큼 늘려주는 메소드"""
         self.balance += amount
 
-    def withdraw(self, amount):
+    def withdraw(self, amount: float) -> None:
         """잔액 인스턴스 변수 balance를 파라미터 amount만큼 줄여주는 메소드"""
         if self.balance < amount:
             print("Insufficient balance!")
         else:
             self.balance -= amount
 
-    def add_interest(self):
+    def add_interest(self) -> None:
         """잔액 인스턴스 변수 balance를 이자율만큼 늘려주는 메소드"""
         self.balance *= 1 + BankAccount.interest
 
-# 클래스를 어떻게 사용할 수 있을지 직관적으로 알 수 있다!
+bank_account_1 = BankAccount("Hong Gildong", "1000")
+
+print(bank_account_1.balance)
+#--==>> 1000
+bank_account_1.deposit("00")
+print(bank_account_1.balance)
+#--==>> 100000
+# 타입 힌팅을 했다고 알맞지 않은 타입이 들어와도 에러가 나진 않는다.
