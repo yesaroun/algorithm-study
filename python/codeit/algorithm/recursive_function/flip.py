ea3eee46f2a6ae2aaf1ed917b1a898258c2cd6ff
akor11lyt <akor1@naver.com>
2022-10-02 00:51:27 +0900

    flip

--- python/codeit/algorithm/recursive_function/flip.py
+++ python/codeit/algorithm/recursive_function/flip.py
@@ -2,14 +2,12 @@
 
 # 파라미터 some_list를 거꾸로 뒤집는 함수
 def flip(some_list):
-    if len(some_list) == 1 or len(some_list) == 0:
-
-
+    # base case
+    if len(some_list) == 0 or len(some_list) == 1:
         return some_list
-    result = []
-    if some_list:
-        result.append(some_list.pop())
-        flip(some_list)
+
+    # recursive case
+    return some_list[-1:] + flip(some_list[:-1])
 
 # 테스트
 some_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
