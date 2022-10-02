7e5a9a2b7e92b7c3f00919be5585a9be0f9120de
akor11lyt <akor1@naver.com>
2022-10-03 00:25:05 +0900

    infix

---
+++ python/data_structures/ch09/infix2.py
@@ -0,0 +1,43 @@
+from stacks import Stack
+
+class Infix:
+    OPS = "+", "-", "*", "/", "(", ")"
+    Priority = {'+': 1, '-': 1, '*': 2, '/': 2}
+
+    def __init__(self, expr):
+        (*self.expr,) = expr
+
+    def translate_postfix(self):
+        stack = Stack(len(self.expr))
+
+        list_ = []
+        for token in self.expr:
+            if token not in Infix.OPS:
+                list_.append(token)
+            elif token == "(":
+                stack.push(token)
+            elif token == ")":
+                while not stack.is_empty() and stack.peek() != "(":
+                    list_.append(stack.peek())
+                    stack.pop()
+                stack.pop()
+            elif stack.is_empty():
+                stack.push(token)
+            else:
+                while not stack.is_empty() and stack.peek() != "(" and Infix.Priority[token] <= Infix.Priority[stack.peek()]:
+                    list_.append(stack.peek())
+                    stack.pop()
+                stack.push(token)
+
+        while not stack.is_empty():
+            list_.append(stack.peek())
+            stack.pop()
+
+
+        return "".join(list_)
+
+if __name__ == "__main__":
+    expr = "a*(b+c)*d"
+    infix = Infix(expr)
+    postfix = infix.translate_postfix()
+    print(postfix)
