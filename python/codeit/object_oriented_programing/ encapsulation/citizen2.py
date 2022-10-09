691bab17efffd992274dd359647dc281a4015e46
akor11lyt <akor1@naver.com>
2022-10-09 23:02:31 +0900

    encapsulation

--- python/codeit/object_oriented_programing/ encapsulation/citizen2.py
+++ python/codeit/object_oriented_programing/ encapsulation/citizen2.py
@@ -1,6 +1,6 @@
 class Citizen:
     """주민 클래스"""
-    drinking_age = 19 # 음주 가능 나이
+    drinking_age = 19
 
     def __init__(self, name, age, resident_id):
         """이름, 나이, 주민등록번호"""
@@ -8,7 +8,6 @@ class Citizen:
         self.set_age(age)
         self.__resident_id = resident_id
 
-    # def __authenticate(self, id_field):
     def authenticate(self, id_field):
         """본인이 맞는지 확인하는 메소드"""
         return self.__resident_id == id_field
@@ -33,16 +32,5 @@ class Citizen:
         else:
             self.__age = value
 
-young = Citizen("young", -10, "12345")
-#--==>> 나이는 0보다 작을 수 없습니다. 기본 값 0으로 나이를 설정하겠습니다.
-print(young.get_age())
-#--==>> 0
-
-young.set_age(-8)
-#--==>> 나이는 0보다 작을 수 없습니다. 기본 값 0으로 나이를 설정하겠습니다.
-print(young.get_age())
-#--==>> 0
-
-young.set_age(10)
-print(young.get_age())
-#--==>> 10
+person1 = Citizen("kim", 15, "1234")    # (1)
+print(dir(person1))                     # (2)
