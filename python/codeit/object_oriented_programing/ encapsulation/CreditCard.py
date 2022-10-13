59dc6381a1a8ebbb1e63649d524d5911e5611d7b
akor11lyt <akor1@naver.com>
2022-10-14 01:33:37 +0900

    캡슐화

--- python/codeit/object_oriented_programing/ encapsulation/CreditCard.py
+++ python/codeit/object_oriented_programing/ encapsulation/CreditCard.py
@@ -2,32 +2,40 @@ class CreditCard:
     MAX_PAYMENT_LIMIT = 30000000
 
     def __init__(self, name, password, payment_limit):
-        self.__name = name
-        self.__password = password
-        self.__payment_limit = payment_limit
+        self.name = name
+        self._password = password
+        self._payment_limit = payment_limit
 
-    def get_name(self, name):
-        self.__name = name
+    @property
+    def password(self):
+        return "비밀 번호는 볼 수 없습니다"
 
-    def set_name(self):
-        return self.__name
+    @password.setter
+    def password(self, password):
+        self._password = password
 
-    def get_password(self, password):
-        self.__password = password
+    @property
+    def payment_limit(self):
+        return self._payment_limit
 
-    def set_pa
+    @payment_limit.setter
+    def payment_limit(self, payment_limit):
+        if payment_limit > 0 and payment_limit < CreditCard.MAX_PAYMENT_LIMIT:
+            self._payment_limit = payment_limit
+        else:
+            print("카드 한도는 0원 ~ 3천만 원 사이로 설정해주세요!")
 
 
 card = CreditCard("강영훈", "123", 100000)
 
-print(card.get_name())
-print(card.get_password())
-print(card.get_payment_limit())
+print(card.name)
+print(card.password)
+print(card.payment_limit)
 
-card.set_name("성태호")
-card.set_password("1234")
-card.set_payment_limit(-10)
+card.name = "성태호"
+card.password = "1234"
+card.payment_limit = -10
 
-print(card.get_name())
-print(card.get_password())
-print(card.get_payment_limit())
+print(card.name)
+print(card.password)
+print(card.payment_limit)
