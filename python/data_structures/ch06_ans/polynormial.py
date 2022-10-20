79fd8b64cbfacdeb1aab63ae3fb879c23730b364
akor11lyt <akor1@naver.com>
2022-10-21 00:22:45 +0900

    polynormial

--- python/data_structures/ch06_ans/polynormial.py
+++ python/data_structures/ch06_ans/polynormial.py
@@ -6,7 +6,18 @@ class Polynomial:
         self.coef = [0] * (self.degree + 1)
 
     def get_lead_exp(self):
-        raise NotImplemented
+        i = len(self.coef) - 1
+        while i >= 0 and self.coef[i] == 0:
+            i -= 1
+    # 추가하기!
+
+
+
+
+
+
+
+
 
     def evaluate(self, x):
         raise NotImplemented
@@ -26,3 +37,32 @@ class Polynomial:
 
     def remove(self, exp):
         self.coef[exp] = 0
+
+    def __str__(self):
+        ret = ""
+        for coef, exp in [
+            (self.coef[i], i) for i in range(self.degree) if self.coef[i] != 0
+        ][
+            ::-1
+        ]:
+            ret += f"({coef})x^{exp} + "
+
+        return f"{ret}\b\b"
+
+if __name__ == "__main__":
+
+    poly = Polynomial(20)
+    poly.attach(3, 20).attach(2, 5).attach(4, 0)
+    print(poly)
+
+    x = 3
+    res = poly.evaluate(x)
+    print(f"{poly} = {res}, where xi = {x}")
+
+    print(poly.get_lead_exp())
+#--==>>
+"""
+(3)x^20 + (2)x^5 + (4)x^0
+(3)x^20 + (2)x^5 + (4)x^0 = 10460353693, where x = 3 
+20
+"""
