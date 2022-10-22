573bd6a3606f76552ad9d96adf8cf7f041bae65e
akor11lyt <akor1@naver.com>
2022-10-22 17:18:04 +0900

    modules

--- python/codeit/modules/run.py
+++ python/codeit/modules/run.py
@@ -13,4 +13,6 @@ print(square(3))
 # 하지만 지금처럼 이름이 중복되면 바람직하지 않다
 # 그래서 한 네임스페이스 안에는 같은 이름이 중복되지 않는게 좋다.
 # 중복되지 않게 하는 첫 번째 방법은 square 함수를 가져올 때 이름을 바꿔 주는 것이다.
+# from area import square as sq
 # 다른 방법은 모듈 그대로 import 하는 방법이다.
+# import area
