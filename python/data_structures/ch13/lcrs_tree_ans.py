e55ddef2cec40ae4aede524fc9f71c9b076c1142
akor11lyt <akor1@naver.com>
2022-10-25 14:12:37 +0900

    lcrs tree

---
+++ python/data_structures/ch13/lcrs_tree_ans.py
@@ -0,0 +1,79 @@
+from stacks import Stack
+
+class Tree:
+    def __init__(self):
+        self.root = None
+
+    def build(self, sexpr):
+        stack = Stack()
+        it = iter(sexpr)
+
+        root = None
+        # stack이 empty이거나 it가 있는 동안 반복
+        while stack.is_empty() or it:
+            try:
+                token = next(it)
+            except StopIteration:
+                break
+
+            # ) 가 아니면 TreeNode 객체로 stack에 저장해라
+            if token != ")":
+                stack.push(self.TreeNode(token))
+                continue
+
+            prev = None
+            # ( 가 아니면 stack에 있는걸 빼서
+            # 뺀거의 형제 노드를 prev로 정하고
+            # 뺀 걸 prev에 저장한다
+            # 그리고 이걸 반복한다
+            while stack.peek().elem != "(":
+                node = stack.peek()
+                stack.pop()
+                node.right_sibling = prev
+                prev = node
+
+
+    class TreeNode:
+        def __init__(self, elem):
+            self.elem = elem
+            self.left_child = None
+            self.right_sibling = None
+
+        def __repr__(self):
+            return str(self)
+
+        def __str__(self):
+            return f"{self.elem}"
+
+
+if __name__ == "__main__":
+
+    sexpr = "( A ( B ( E ( K L ) F ) C ( G ) D ( H ( M ) I J ) ) )"
+    sexpr = sexpr.split()
+
+    tree = Tree()
+    tree.build(sexpr)
+    root = tree.root
+    print(root)
+    b = root.left_child
+    print(b)
+    e = b.left_child
+    print(e)
+    k = e.left_child
+    print(k)
+    l = k.right_sibling
+    print(l)
+    f = e.right_sibling
+    print(f)
+    c = b.right_sibling
+    print(c)
+    d = c.right_sibling
+    print(d)
+    g = c.left_child
+    print(g)
+    h = d.left_child
+    print(h)
+    i = h.right_sibling
+    print(i)
+    j = i.right_sibling
+    print(j)
