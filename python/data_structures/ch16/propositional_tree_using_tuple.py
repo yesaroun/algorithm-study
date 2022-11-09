84ff29607ec0b732665dfd74f9acc4db1b20cfe7
akor11lyt <akor1@naver.com>
2022-11-09 09:20:11 +0900

    tree

---
+++ python/data_structures/ch16/propositional_tree_using_tuple.py
@@ -0,0 +1,96 @@
+from stacks import Stack
+
+
+class BTreeBuilder:
+    @staticmethod
+    def build(sexpr):
+        root = None
+
+        # 메인 프로시저를 위한 스택
+        stack_proc = Stack()
+        # 서브트리를 위한 스택
+        stack_subtree = Stack()
+
+        for expr in sexpr:
+            if expr != ")":
+                stack_proc.push(TreeNode(expr))
+                continue
+
+            # case ")"
+            # ( A ( B C
+            while stack_proc.peek().elem != "(":
+                node = stack_proc.peek()
+                stack_proc.pop()
+                # print(node)
+                node = node if node.elem != "#" else None
+                stack_subtree.push(node)
+
+            # print(stack_subtree)
+
+            stack_proc.pop()    # remove "("
+            # print(stack_proc)
+            # stack에 마지막 하나만(root) 들어가 있는 경우 ( A )
+            if stack_proc.is_empty():
+                root = stack_subtree.peek()
+                stack_subtree.pop()
+            else:
+                root = stack_proc.peek()
+                stack_proc.pop()
+
+            if stack_subtree.is_empty():
+                continue
+
+            root.left_child = stack_subtree.peek()
+            stack_subtree.pop()
+            root.right_child = stack_subtree.peek()
+            stack_subtree.pop()
+            stack_proc.push(root)
+
+        # 기술한 sexpr 이 잘못되었을때 나오는 에러
+        if not stack_proc.is_empty():
+            raise Exception("expression is wrong.")
+
+        return root
+
+class TreeNode:
+    def __init__(self, elem):
+        self.elem = elem
+        self.value = None  # boolean value
+        self.left_child = self.right_child = None
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return f"{self.elem}"
+
+class PropositionalTree:
+    def __init__(self, root):
+        self.root = root
+
+    def calculate_propositional(self, *param):
+        ret = None
+
+        def calculate_recursive(root):
+            if root is None:
+                return
+            calculate_recursive(root.left_child)
+            calculate_recursive(root.right_child)
+
+            if str.isdigit(root.elem):
+                root.elem = param[int(root.elem)]
+
+            if root.elem == "AND":
+                root.value =
+
+        calculate_recursive(self.root)
+        return ret
+
+
+if __name__ == "__main__":
+    sexpr = "( OR ( OR ( AND ( 0 NOT ( # 1 ) ) AND ( NOT ( # 0 ) 2 ) ) NOT ( # 2 ) ) )".split()
+    root = BTreeBuilder.build(sexpr)
+    tree = PropositionalTree(root)
+
+    prop = tree.calculate_propositional(False, True, False)
+    print(prop.value)
