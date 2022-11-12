7aa0d016dae8a3f565bdb873a91c4d435ed412a4
akor11lyt <akor1@naver.com>
2022-11-12 18:34:52 +0900

    heapify

--- python/codeit/tree/heap/heapify.py
+++ python/codeit/tree/heap/heapify.py
@@ -8,23 +8,24 @@ def swap(tree, index_1, index_2):
 def heapify(tree, index, tree_size):
     """heapify 함수"""
 
-
-
     # 왼쪽 자식 노드의 인덱스와 오른쪽 자식 노드의 인덱스를 계산
     left_child_index = 2 * index
     right_child_index = 2 * index + 1
 
-    if tree[left_child_index] is None and tree[right_child_index] is None:
-        return
+    largest = index     # 일단 부모 노드의 값이 가장 크다고 설정
 
-    if tree[index] > tree[left_child_index] and tree[index] > tree[right_child_index]:
-        return
-    elif tree[left_child_index] > tree[index]:
-        swap(tree, index, left_child_index)
-        heapify(tree, left_child_index, tree_size)
-    elif tree[right_child_index] > tree[index]:
-        swap(tree, index, right_child_index)
-        heapify(tree, left_child_index, tree_size)
+    # 왼쪽 자식 노드의 값과 비교
+    if 0 < left_child_index < tree_size and tree[largest] < tree[left_child_index]:
+        largest = left_child_index
+
+    # 오른쪽 자식 노드의 값과 비교
+    if 0 < right_child_index < tree_size and tree[largest] < tree[right_child_index]:
+        largest = right_child_index
+
+    if largest != index:            # 부모 노드의 값이 자식 노드의 값보다 작으면
+        swap(tree, index, largest)  # 부모 노드와 최댓값을 가진 자식 노드의 위치를 바꾼다.
+        heapify(tree, largest, tree_size)
+        # 자리가 바뀌어 자식 노드가 된 기존의 부모 노드를 대상으로 또 heapify 함수를 호출
 
 
 # 실행 코드
