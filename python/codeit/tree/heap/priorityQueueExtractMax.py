20a9fb2bb530a4ad9cd6dd414e568c33d17411a6
akor11lyt <akor1@naver.com>
2022-11-14 08:54:49 +0900

    힙 우선순위 데이터 추출

---
+++ python/codeit/tree/heap/priorityQueueExtractMax.py
@@ -0,0 +1,47 @@
+from heapify_code import *
+
+class PriorityQueue:
+    """힙으로 구현한 우선순위 큐"""
+    def __init__(self):
+        self.heap = [None]  # 파이썬 리스트로 구현한 힙
+
+    def insert(self, data):
+        """삽입 메소드"""
+        self.heap.append(data)  # 힙의 마지막에 데이터 추가
+        reverse_heapify(self.heap, len(self.heap)-1) # 삽입된 노드(추가된 데이터)의 위치를 재배치
+
+    def extract_max(self):
+        """최우선순위 데이터 추출 메소드"""
+        if self.heap[1] is not None:
+            swap(self.heap, 1, len(self.heap) - 1)
+            return_value = self.heap.pop()
+            heapify(self.heap, 1, len(self.heap))
+
+            return return_value
+
+        else:
+            raise Exception
+
+    def __str__(self):
+        return str(self.heap)
+
+# 출력 코드
+priority_queue = PriorityQueue()
+
+priority_queue.insert(6)
+priority_queue.insert(9)
+priority_queue.insert(1)
+priority_queue.insert(3)
+priority_queue.insert(10)
+priority_queue.insert(11)
+priority_queue.insert(13)
+
+print(priority_queue.extract_max())
+print(priority_queue.extract_max())
+print(priority_queue.extract_max())
+print(priority_queue.extract_max())
+print(priority_queue.extract_max())
+print(priority_queue.extract_max())
+print(priority_queue.extract_max())
+
+print("13\n11\n10\n9\n6\n3\n1")
