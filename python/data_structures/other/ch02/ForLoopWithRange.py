7feed7d6ce80a189548628c4aafbbbc1944c8f62
akor11lyt <akor1@naver.com>
2022-11-18 00:29:11 +0900

    삭제

--- python/data_structures/other/ch02/ForLoopWithRange.py
+++
@@ -1,8 +0,0 @@
-# for-loop with range()
-n = int(input('input n to calculate sum of [0..n] : '))
-nSum = 0
-
-for i in range(0, n+1):     #sSum = sum(range(0, n+1))
-    nSum += i
-
-print("Sum of [0..%d] = %d" %(n, nSum))
