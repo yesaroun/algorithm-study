7ef28b904b978d3fbd0c8e48250c6e24c464b652
akor11lyt <akor1@naver.com>
2022-11-25 18:16:35 +0900

    left-child right-sibling tree

--- python/data_structures/ch13/lcrs_tree_ans_2.py
+++ python/data_structures/ch13/lcrs_tree_ans_2.py
@@ -9,23 +9,17 @@ class Tree:
         it = iter(sexpr)
 
         root = None
-        # stack이 empty이거나 it가 있는 동안 반복
         while stack.is_empty() or it:
             try:
                 token = next(it)
             except StopIteration:
                 break
 
-            # ) 가 아니면 TreeNode 객체로 stack에 저장해라
             if token != ")":
                 stack.push(self.TreeNode(token))
                 continue
 
             prev = None
-            # ( 가 아니면 stack에 있는걸 빼서
-            # 뺀거의 형제 노드를 prev로 정하고
-            # 뺀 걸 prev에 저장한다
-            # 그리고 이걸 반복한다
             while stack.peek().elem != "(":
                 node = stack.peek()
                 stack.pop()
@@ -33,25 +27,20 @@ class Tree:
                 prev = node
 
             stack.pop()
-            # "(" pop
 
-            # 이 경우는 root인 경우 while문 나가기
             if stack.is_empty():
                 root = prev
                 continue
 
             root = stack.peek()
-            # ( 앞에 있는 문자를 root에 저장 및 left_child 저장
             root.left_child = prev
             stack.pop()
             stack.push(root)
-            # stack에 root 저장하고 뒤에 반복
 
         if not stack.is_empty():
             raise Exception("expression is wrong.")
 
         self.root = root
-
 
     class TreeNode:
         def __init__(self, elem):
@@ -67,7 +56,6 @@ class Tree:
 
 
 if __name__ == "__main__":
-
     sexpr = "( A ( B ( E ( K L ) F ) C ( G ) D ( H ( M ) I J ) ) )"
     sexpr = sexpr.split()
 
