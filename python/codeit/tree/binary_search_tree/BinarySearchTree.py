9b6716384665f6b3cb4b6ab9c04198a9bd3dfbb5
akor11lyt <akor1@naver.com>
2022-11-29 23:23:02 +0900

    이진트리

--- python/codeit/tree/binary_search_tree/BinarySearchTree.py
+++ python/codeit/tree/binary_search_tree/BinarySearchTree.py
@@ -6,6 +6,7 @@ class Node:
         self.left_child = None
         self.right_child = None
 
+
 def print_inorder(node):
     """주어진 노드를 in-order로 출력해주는 함수"""
     if node is not None:
@@ -19,6 +20,65 @@ class BinarySearchTree:
     def __init__(self):
         self.root = None
 
+
+    def delete(self, data):
+        """이진 탐색 트리 삭제 메소드"""
+        node_to_delete = self.search(data)      # 삭제할 노드를 가지고 온다
+        parent_node = node_to_delete.parent     # 삭제할 노드의 부모 노드 
+
+        # 경우 1: 지우려는 노드가 leaf 노드일 때
+        if node_to_delete.left_child is None and node_to_delete.right_child is None:
+            # 지우려는 노드가 root 노드일 때
+            if self.root is node_to_delete:
+                self.root = None
+            else:   # 일반적인 경우
+                if parent_node.left_child is node_to_delete:
+                    parent_node.left_child = None
+                elif parent_node.right_child is node_to_delete:
+                    parent_node.right_child = None
+
+        # 경우 2: 지우려는 노드가 자식이 하나인 노드일 때:
+        elif node_to_delete.left_child is None:     # 지우려는 노드가 오른쪽 자식만 있을 때:
+            # 지우려는 노드가 root 노드일 때
+            if node_to_delete is self.root:
+                self.root = node_to_delete.right_child
+                self.root.parent = None
+            # 지우려는 노드가 부모의 왼쪽 자식일 때
+            elif node_to_delete is parent_node.left_child:
+                parent_node.left_child = node_to_delete.right_child
+                node_to_delete.right_child.parent = parent_node
+            # 지우려는 노드가 부모의 오른쪽 자식일 때
+            else:
+                parent_node.right_child = node_to_delete.right_child
+                node_to_delete.right_child.parent = parent_node
+        elif node_to_delete.right_child is None:    # 지우려는 노드가 왼쪽 자식만 있을 때:
+            # 지우려는 노드가 root 노드일 때
+            if node_to_delete is self.root:
+                self.root = node_to_delete.left_child
+                self.root.left_child.parent = None
+            # 지우려는 노드가 부모의 왼쪽 자식일 때
+            elif node_to_delete is parent_node.left_child:
+                parent_node.left_child = node_to_delete.left_child
+                node_to_delete.left_child.parent = parent_node
+            # 지우려는 노드가 부모의 오른쪽 자식일 때
+            else:
+                parent_node.right_child = node_to_delete.left_child
+                node_to_delete.left_child.parent = parent_node
+
+        # 경우 3: 지우려는 노드가 2개의 자식이 있을 때
+        else:
+            successor = self.find_min(node_to_delete.right_child)   # 삭제하려는 노드의 successor 노드 받아오기
+            node_to_delete.data = successor.data    # 삭제하려는 노드의 데이터에 successor의 데이터 저장
+            # successor 노드 트리에서 삭제
+            if successor is successor.parent.left_child:        # successor 노드가 어떤 부모 노드의 왼쪽 자식일 때
+                successor.parent.left_child = successor.right_child
+            else:   # successor 노드가 삭제하려는 노드의 바로 오른쪽 자식일 때
+                successor.parent.right_child = successor.right_child
+
+            if successor.right_child is not None:       # successor 노드가 오른쪽 자식이 있을 때
+                successor.right_child.parent = successor.parent
+                        
+
     @staticmethod
     def find_min(node):
         """(부분)이진 탐색 트리의 가장 작은 노드 리턴"""
@@ -29,6 +89,7 @@ class BinarySearchTree:
             temp = temp.left_child
 
         return temp
+
 
     def search(self, data):
         """이진 탐색 트리 탐색 메소드, 찾는 데이터를 갖는 노드가 없으면 None을 리턴한다"""
@@ -46,6 +107,7 @@ class BinarySearchTree:
                 temp = temp.left_child
 
         return None # 원하는 데이터가 트리에 없으면 None 리턴 
+
 
     def insert(self, data):
         new_node = Node(data)       # 삽입할 데이터를 갖는 새 노드 생성
@@ -84,6 +146,7 @@ class BinarySearchTree:
         print_inorder(self.root)    # root 노드를 in-order로 출력한다
 
 
+
 # 비어 있는 이진 탐색 트리 생성
 bst = BinarySearchTree()
 
@@ -100,5 +163,8 @@ bst.insert(2)
 bst.insert(4)
 bst.insert(14)
 
-print(bst.find_min(bst.root).data)  # 전체 이진 탐색 트리에서 가장 작은 노드
-print(bst.find_min(bst.root.right_child).data)  # root 노드의 오른쪽 부분 트리에서 가장 작은 노드
+# 자식이 두 개 다 있는 노드 삭제
+bst.delete(7)
+bst.delete(11)
+
+bst.print_sorted_tree()
