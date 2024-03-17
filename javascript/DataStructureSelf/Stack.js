f00b4d9b950d99c1485a856cc8e083a7ef59d225
yesaroun <akor1@naver.com>
2024-03-17 23:20:39 +0900

    js 알고리즘

---
+++ javascript/DataStructureSelf/Stack.js
@@ -0,0 +1,7 @@
+class Node {
+  constructor(item) {
+    this.item = item;
+    this.next = next;
+  }
+}
+
---
+++ python/codeit/paradigm/divide_and_conquer/merge_sort.py
@@ -0,0 +1,13 @@
+def merge(list1: list[int], list2: list[int]) -> list[int]:
+    i = j = 0
+    merged_list: list[int] = []
+    while i < len(list1) and 
+
+# 합병 정렬
+def merge_sort(my_list):
+    pass
+
+# 테스트 코드
+print(merge_sort([1, 3, 5, 7, 9, 11, 13, 11]))
+print(merge_sort([28, 13, 9, 30, 1, 48, 5, 7, 15]))
+print(merge_sort([2, 5, 6, 7, 1, 2, 4, 7, 10, 11, 4, 15, 13, 1, 6, 4]))
