e8a9f7658902bbb3c6dd808fffb54120458e9670
yesaroun <akor1@naver.com>
2024-05-20 11:10:48 +0900

    [feat] design browser history

---
+++ python/leetcode/LinkedList/1472DesignBrowserHistory.py
@@ -0,0 +1,64 @@
+# https://leetcode.com/problems/design-browser-history/description/
+
+
+class ListNode(object):
+    def __init__(self, val=0, next=None, prev=None):
+        self.val = val
+        self.next = next
+        self.prev = prev
+
+
+class BrowserHistory(object):
+    def __init__(self, homepage):
+        """
+        :type homepage: str
+        """
+        self.head = self.current = ListNode(val=homepage)
+
+    def visit(self, url):
+        """
+        :type url: str
+        :rtype: None
+        """
+        self.current.next = ListNode(val=url, prev=self.current)
+        self.current = self.current.next
+        return None
+
+    def back(self, steps):
+        """
+        :type steps: int
+        :rtype: str
+        """
+        while steps > 0 and self.current.prev != None:
+            steps -= 1
+            self.current = self.current.prev
+        return self.current.val
+
+    def forward(self, steps):
+        """
+        :type steps: int
+        :rtype: str
+        """
+        while steps > 0 and self.current.next != None:
+            steps -= 1
+            self.current = self.current.next
+        return self.current.val
+
+
+# Your BrowserHistory object will be instantiated and called as such:
+# obj = BrowserHistory(homepage)
+# obj.visit(url)
+# param_2 = obj.back(steps)
+# param_3 = obj.forward(steps)
+
+browserHistory = BrowserHistory("leetcode.com")
+browserHistory.visit("google.com")
+browserHistory.visit("facebook.com")
+browserHistory.visit("youtube.com")
+browserHistory.back(1)
+browserHistory.back(1)
+browserHistory.forward(1)
+browserHistory.visit("linkedin.com")
+browserHistory.forward(2)
+browserHistory.back(2)
+browserHistory.back(7)
